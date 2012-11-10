2afb447330686ef4cb205b45093758483996a01d
Danny Gratzer <danny.gratzer@gmail.com>
2012-11-10 11:21:27 -0600

    Added a simple file based handler, file_handler.

---
+++ src/handlers/file_handler.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <stdexcept>
+#include "file_handler.hpp"
+
+using namespace al;
+
+file_handler::file_handler(const std::string& file):stream(file.c_str()){
+  if(!stream)
+    throw std::runtime_error("Unable to open file: " + file);
+}
+
+void file_handler::handle(level l, const std::string& name, const std::string& message){
+  stream<<to_string(l)<<','<<name<<','<<message<<std::endl;
+}
---
+++ src/handlers/file_handler.hpp
@@ -0,0 +1,32 @@
+#ifndef FILE_HANDLER_H
+#define FILE_HANDLER_H
+#include <fstream> //A simple header which has forward declarations of all streams
+#include "handler.hpp"
+
+namespace al{
+
+  /**
+   * @Class file_handler
+   * @brief The classic file logger, based on std::ofstream
+   */
+  class file_handler : public handler{
+    public:
+      /**
+       * @param file The file name for the log file.
+       * @exception std::runtime_error If the file is unable to be opened.
+       */
+      file_handler(const std::string& file);
+
+      /**
+       * Prints the message to the log file in csv form.
+       *
+       * "level,name,message"
+       */
+      void handle(level, const std::string&, const std::string&);
+
+    private:
+      std::ofstream stream;
+  };
+}
+
+#endif
