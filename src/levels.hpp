e155599debafc1cae13ed854e3f9aa983b78b4a3
Danny Gratzer <danny.gratzer@gmail.com>
2012-11-10 00:23:30 -0600

    Added levels.hpp and logger_creator.hpp
    levels.hpp defines an enum class which provides the various levels of log messages.
    
    logger_creator is a class which is used to configure loggers before spawning them.

---
+++ src/levels.hpp
@@ -0,0 +1,14 @@
+#ifndef LEVELS_H
+#define LEVELS_H
+
+enum class level{
+  none,
+  debug,
+  info,
+  warning,
+  nonfatal_error,
+  fatal_error,
+  all
+};
+
+#endif
---
+++ src/logger_creator.hpp
@@ -0,0 +1,35 @@
+#ifndef LOGGER_CREATOR_H
+#define LOGGER_CREATOR_H
+
+#include <list>
+#include <memory>
+#include "logger.hpp"
+#include "handler.hpp"
+#include "levels.hpp"
+
+class logger_creator{
+  public:
+    logger_creator():ignore_level(level::none) {}
+
+    logger_creator& add_handler(std::shared_ptr<handler> new_handler){
+      handler_list.push_back(new_handler);
+      return *this;
+    }
+
+    logger_creator& set_ignore(level level){
+      ignore_level = level;
+      return *this;
+    }
+
+    logger spawn(std::string&& name){
+      return logger(handler_list, ignore_level, name); 
+    }
+
+
+    virtual ~logger_creator(){}
+  protected:
+    std::list<std::shared_ptr<handler>> handler_list;
+    level ignore_level;
+};
+
+#endif
