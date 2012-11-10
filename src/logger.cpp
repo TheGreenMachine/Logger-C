517c2646981530a94a93910686f1121ca3a2a257
Danny Gratzer <danny.gratzer@gmail.com>
2012-11-10 01:06:32 -0600

    Added logger.cpp and logger.hpp
    These define the class logger, which is spawned by logger_creator's and is used to actually log messages.

---
+++ src/logger.cpp
@@ -0,0 +1,28 @@
+#include "logger.hpp"
+
+void logger::log( level l, const std::string& message){
+  if(l<ignore_level){
+    return;
+  }
+
+  for(auto h : handler_list){
+    h->handle(l, name, message);
+  }
+}
+
+logger& logger::operator<<(level l){
+  current_level = l;
+  return *this;
+}
+
+logger& logger::operator<<(const std::string& input){
+  message+=input;
+  return *this;
+}
+
+logger& logger::operator<<(end){
+  log(current_level, message);
+  current_level = level::info;
+  message = "";
+  return *this;
+}
---
+++ src/logger.hpp
@@ -0,0 +1,32 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <list>
+#include <string>
+#include "levels.hpp"
+#include "handler.hpp"
+
+class end{};
+
+class logger{
+  public:
+    logger(const std::list<std::shared_ptr<handler>>& handlers, level ignore,
+        const std::string& logger_name):
+      ignore_level(ignore), handler_list(handlers), name(logger_name), current_level(level::info){}
+
+    void log(level, const std::string&);
+
+    logger& operator<<(level);
+    logger& operator<<(const std::string&);
+    logger& operator<<(end);
+
+  private:
+      level ignore_level;
+      const std::list<std::shared_ptr<handler>>& handler_list;
+      const std::string name;
+
+      std::string message;
+      level current_level;
+};
+
+#endif
