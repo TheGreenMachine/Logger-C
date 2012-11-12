2d8d48273e675a2cb6946fb9ba6e6d3622a31a62
Danny Gratzer <danny.gratzer@gmail.com>
2012-11-11 22:10:57 -0600

    Added clog_handler.hpp, Identical to cerr_handler except for clog

---
+++ src/handlers/clog_handler.hpp
@@ -0,0 +1,29 @@
+#ifndef CLOG_HANDLER_H
+#define CLOG_HANDLER_H
+
+#include "handler.hpp"
+#include <iostream>
+
+namespace al{
+  /**
+   * @Class cerr_handle
+   * @brief A simple handler, it prints all log entries to clog.
+   *
+   * The messages are of the form "(level) time, name : message" for easy readability.
+   */
+  class cerr_handler : public handler{
+    public: 
+      void handle(level l, const std::string& name, const std::string& message){
+        std::clog<<"("<< to_string(l) << ") "<<name<<" : "<<message<<std::endl;
+      }
+
+      void handle(level l, const std::string& time, const std::string& name, const std::string& message){
+        std::clog<<"("<< to_string(l) << ") "<<time<<", "<<name<<" : "<<message<<std::endl;
+      }
+
+      void handle(const std::string& message){
+        std::clog<<message<<std::endl;
+      }
+  };
+}
+#endif
