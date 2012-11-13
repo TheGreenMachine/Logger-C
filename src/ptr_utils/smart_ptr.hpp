9a1f084fee35add6d223d228d2d2ebe8cebb05b3
Danny Gratzer <danny.gratzer@gmail.com>
2012-11-13 14:15:24 -0600

    Added a smart pointer implementation smart_ptr.
    This will be used to replace std::shared_ptr throughout the code.

---
+++ src/ptr_utils/smart_ptr.hpp
@@ -0,0 +1,61 @@
+#ifndef SMART_PTR_H
+#define SMART_PTR_H
+
+template<typename T>
+class smart_ptr{
+  public:
+
+    //Constructors
+    smart_ptr(T* p):copies(new int(1)), ptr(p)
+    {}
+    smart_ptr(const T& ins):copies(new int(1)), ptr(new T(ins))
+    {}
+    smart_ptr(smart_ptr<T>& cp):copies(cp.copies),
+                                    ptr(cp.ptr)
+    {
+      ++(*copies);
+    }
+
+    //Operators
+    smart_ptr& operator=(const smart_ptr<T>& cp){
+      handle_destruction();
+      copies = cp.copies;
+      ++(*copies);
+      ptr = cp.ptr;
+      return *this;
+    }
+
+    T& operator*() const{
+      return *ptr;
+    }
+
+    T* operator->() const{
+      return ptr;
+    }
+
+    //Functions
+    operator bool(){
+      return ptr;
+    }
+
+    int instances(){
+      return *copies;
+    }
+
+    ~smart_ptr(){
+      handle_destruction();
+    }
+
+  private:
+    int* copies;
+    T* ptr;
+
+    void handle_destruction(){
+      if(--(*copies) == 0){
+        delete ptr;
+        delete copies;
+      }
+    }
+};
+
+#endif
